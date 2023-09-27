679edf0c176322c4a6ea53f8ab7666233326b312
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-09-27 11:39:53 +0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.application.housingloancalculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.application.housingloancalculator to javafx.fxml;
+    exports com.application.housingloancalculator;
+}
