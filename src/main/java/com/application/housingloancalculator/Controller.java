6f522beee949c1bc34de7b5b0b0e9bbaca912833
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-09-29 16:33:33 +0300

    input

--- src/main/java/com/application/housingloancalculator/Controller.java
+++ src/main/java/com/application/housingloancalculator/Controller.java
@@ -1,6 +1,37 @@
 package com.application.housingloancalculator;
 
-public class Controller {
+import com.application.housingloancalculator.charts.ChartType;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class Controller implements Initializable {
+    @FXML
+    private TextField dealAmountTextField;
+    @FXML
+    private TextField annualInterestTextField;
+    @FXML
+    private TextField yearsTextField;
+    @FXML
+    private TextField monthsTextField;
+    @FXML
+    private ChoiceBox<ChartType> chartTypeChoiceBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        chartTypeChoiceBox.getItems().add(ChartType.ANNUITY);
+        chartTypeChoiceBox.getItems().add(ChartType.LINEAR);
+    }
+
+    public void calculate(ActionEvent e) {
+        Integer.parseInt(dealAmountTextField.getText());
+        Integer.parseInt(annualInterestTextField.getText());
+        Integer.parseInt(yearsTextField.getText());
+        Integer.parseInt(monthsTextField.getText());
+    }
 }
---
+++ src/main/java/com/application/housingloancalculator/charts/ChartType.java
@@ -0,0 +1,6 @@
+package com.application.housingloancalculator.charts;
+
+public enum ChartType {
+    ANNUITY,
+    LINEAR
+}
