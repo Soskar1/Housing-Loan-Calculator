d77229b2a1d863a746ea495e45431649c0a7c0fe
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-09-29 16:57:41 +0300

    new window creation with button

--- src/main/java/com/application/housingloancalculator/Controller.java
+++ src/main/java/com/application/housingloancalculator/Controller.java
@@ -3,10 +3,14 @@ package com.application.housingloancalculator;
 import com.application.housingloancalculator.charts.ChartType;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -28,10 +32,17 @@ public class Controller implements Initializable {
         chartTypeChoiceBox.getItems().add(ChartType.LINEAR);
     }
 
-    public void calculate(ActionEvent e) {
-        Integer.parseInt(dealAmountTextField.getText());
-        Integer.parseInt(annualInterestTextField.getText());
-        Integer.parseInt(yearsTextField.getText());
-        Integer.parseInt(monthsTextField.getText());
+    public void calculate(ActionEvent e) throws IOException {
+//        Integer.parseInt(dealAmountTextField.getText());
+//        Integer.parseInt(annualInterestTextField.getText());
+//        Integer.parseInt(yearsTextField.getText());
+//        Integer.parseInt(monthsTextField.getText());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("result.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Housing Loan Calculator");
+        stage.setScene(scene);
+        stage.show();
     }
 }
